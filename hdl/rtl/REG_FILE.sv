3ad42aa394875496b084687ec687fe7f85412e9e
Ryan Cramer <ryancramer22@gmail.com>
2026-03-11 13:12:41 -0700

    polished register file

--- hdl/rtl/REG_FILE.sv
+++ hdl/rtl/REG_FILE.sv
@@ -1,23 +1,43 @@
+/* 
+    Module REG_FILE: simple XLEN x XWID register file
+    Instantiation Template:
+
+    // START: REG_FILE
+    REG_FILE #(.XLEN(32),.XWID(32))
+    REGISTER_FILE(
+        .CLK(),
+        .RST(),
+        .LOAD(),
+        .PC_IN(),
+        .PC_OUT()
+    );
+    // END: REG_FILE
+*/
+
 `timescale 1ns/1ps
 
-module REG_FILE(
-    input clk,
-    input [4:0] addr1,
-    input [4:0] addr2,
-    input wen,
-    input [4:0] waddr,
-    input [31:0] wdata,
-    output logic [31:0] rs1,
-    output logic [31:0] rs2
+module REG_FILE
+#(
+    parameter XWID = 32,
+    parameter XLEN = 32
+)(
+    input CLK,
+    input [$clog2(XWID)-1:0] ADDR1,
+    input [$clog2(XWID)-1:0] ADDR2,
+    input WEN,
+    input [$clog2(XWID)-1:0] WADDR,
+    input [XLEN:0] WDATA,
+    output logic [XLEN:0] RS1,
+    output logic [XLEN:0] RS2
 );
 
-    logic [31:0] reg_file [0:31];
+    logic [XLEN:0] reg_file [0:XWID];
 
-    always_ff@(posedge clk)
-        if(wen)
-            reg_file[waddr] <= wdata;
+    always_ff@(posedge CLK)
+        if(WEN)
+            reg_file[WADDR] <= WDATA;
     
-    assign rs1 = reg_file[addr1];
-    assign rs2 = reg_file[addr2];
+    assign RS1 = reg_file[ADDR1];
+    assign RS2 = reg_file[ADDR2];
 
 endmodule
