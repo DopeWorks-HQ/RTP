90b9e51da39946a59201645281949f1af44190ad
Ryan Cramer <ryancramer22@gmail.com>
2026-03-11 09:33:53 -0700

    added prog counter and updates to Makefile

---
+++ hdl/rtl/MUX4T1.sv
@@ -0,0 +1,49 @@
+/*
+    Module: 4 to 1 MUX
+
+    Instantiation Template: 
+
+    // START: MUX4T1
+    MUX4T1 
+    #(.N(32)) 
+    mux4 (
+        .SEL(),
+        .D0(),
+        .D1(),
+        .D2(),
+        .D3(),
+        .DOUT()
+    );
+    // END: MUX4T1
+
+    Author: Ryan Cramer
+    Date: March 4th, 2026
+
+    Description: 
+
+    Module selects DOUT from D0, D1, D2, or D3 
+    based on if SEL is low/high. 
+
+    If SEL = 0, DOUT = D0
+    If SEL = 1, DOUT = D1
+    If SEL = 2, DOUT = D2
+    If SEL = 3, DOUT = D3
+
+*/ 
+
+`timescale 1ns/1ps 
+
+module MUX4T1
+#(parameter N = 32)
+(
+    input [1:0] SEL,
+    input [N-1:0] D0,
+    input [N-1:0] D1,
+    input [N-1:0] D2,
+    input [N-1:0] D3,
+    output [N-1:0] DOUT
+);
+
+    assign DOUT = SEL[1] ? (SEL[0] ? D3 : D1) : (SEL[0] ? D2 : D0); 
+
+endmodule
---
+++ hdl/rtl/PROG_CNTR.sv
@@ -0,0 +1,36 @@
+/* 
+    Module PROG_CNTR: 32 bit Register
+
+    Instantiation Template:
+
+    // START: PROG_CNTR
+    PROG_CNTR #(.XLEN(32))
+    PROGRAM_COUNTER(
+        .CLK(),
+        .RST(),
+        .LOAD(),
+        .PC_IN(),
+        .PC_OUT()
+    );
+    // END: PROG_CNTR
+*/
+
+`timescale 1ns/1ps
+
+module PROG_CNTR
+#(parameter XLEN = 32)
+(
+    input CLK,
+    input RST,
+    input LOAD,
+    input [XLEN-1:0] PC_IN,
+    output [XLEN-1:0] PC_OUT
+);
+
+    always_ff@(posdege CLK)
+        if(RST)
+            PC_OUT <= 0;
+        else if(LOAD)
+            PC_OUT <= PC_IN;
+            
+endmodule
